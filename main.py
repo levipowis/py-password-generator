18031759e01e9ae81b1b738112a002613aa17ca8
Levi Powis <lpowis97@gmail.com>
2024-02-25 16:06:13 -0500

    initial commit - add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+def random_letter_picker(nr_letters):
+    list_length = nr_letters
+    random_letters = []
+    for i in range(0, list_length):
+        random_index = random.randint(0, len(letters) - 1)
+        random_letters.append(letters[random_index])
+    return random_letters
+
+print(random_letter_picker(nr_letters))
